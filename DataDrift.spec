db54c07714f8c1bea94f9826b50d90a91ca5b617
Reginald Erzoah <reginalderzoah10@gmail.com>
2026-06-26 03:35:46 +0000

    updates

--- DataDrift.spec
+++ DataDrift.spec
@@ -1,12 +1,21 @@
 # -*- mode: python ; coding: utf-8 -*-
+from PyInstaller.utils.hooks import collect_all
+
+datas = []
+binaries = []
+hiddenimports = []
+tmp_ret = collect_all('pygame')
+datas += tmp_ret[0]; binaries += tmp_ret[1]; hiddenimports += tmp_ret[2]
+tmp_ret = collect_all('numpy')
+datas += tmp_ret[0]; binaries += tmp_ret[1]; hiddenimports += tmp_ret[2]
 
 
 a = Analysis(
     ['main.py'],
     pathex=[],
-    binaries=[],
-    datas=[],
-    hiddenimports=[],
+    binaries=binaries,
+    datas=datas,
+    hiddenimports=hiddenimports,
     hookspath=[],
     hooksconfig={},
     runtime_hooks=[],
@@ -32,7 +41,6 @@ exe = EXE(
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
-    icon=['assets\\images\\icon.ico'],
 )
 coll = COLLECT(
     exe,
--- datadrift-vscode-extension/extension.js
+++ datadrift-vscode-extension/extension.js
@@ -1,28 +1,59 @@
-const vscode = require("vscode");
-const { execFile } = require("child_process");
-const path = require("path");
+const vscode = require('vscode');
+const { exec } = require('child_process');
+const path = require('path');
 
 function activate(context) {
-    let disposable = vscode.commands.registerCommand(
-        "data-drift.launch",
-        function () {
 
-            const exePath = path.join(
-                __dirname,
-                "release",
-                "v1.0.0",
-                "DataDrift.exe"
-            );
+    console.log("Data Drift Sidebar Extension Active");
 
-            execFile(exePath, (err) => {
-                if (err) {
-                    vscode.window.showErrorMessage("Failed to launch Data Drift");
-                }
-            });
-        }
+    // Command fallback
+    const command = vscode.commands.registerCommand(
+        'data-drift.launch',
+        () => launchGame()
     );
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(command);
+
+    // Sidebar provider
+    const provider = new BreakViewProvider();
+    vscode.window.registerTreeDataProvider('datadriftView', provider);
+}
+
+function launchGame() {
+    // FIX: EXE is in root folder, NOT dist/
+    const exePath = path.join(__dirname, 'DataDrift.exe');
+
+    exec(`"${exePath}"`, (err) => {
+        if (err) {
+            vscode.window.showErrorMessage("Failed to launch DataDrift.exe");
+            console.error(err);
+        }
+    });
+}
+
+class BreakViewProvider {
+    getTreeItem(element) {
+        return element;
+    }
+
+    getChildren() {
+        return [
+            new BreakItem("▶ Play Data Drift")
+        ];
+    }
+}
+
+class BreakItem extends vscode.TreeItem {
+    constructor(label) {
+        super(label);
+
+        this.command = {
+            command: "data-drift.launch",
+            title: "Play Data Drift"
+        };
+
+        this.iconPath = new vscode.ThemeIcon("play");
+    }
 }
 
 function deactivate() {}
